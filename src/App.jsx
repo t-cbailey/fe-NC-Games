479cb6576df94b4953f1a1ca8846953a94200f31
t-cbailey <t.cbaileymusic@gmail.com>
2023-05-30 13:43:09 +0100

    added list of reviews, card4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Reviews from "./components/Reviews";
 
 function App() {
+  const [allReviews, SetAllReviews] = useState([]);
+
   return (
     <>
-      <h1>Hello Home page</h1>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/reviews"
+          element={
+            <Reviews SetAllReviews={SetAllReviews} allReviews={allReviews} />
+          }
+        />
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <section className="welcomeSplash">
+        <h2 className="welcomeText">Welcome</h2>
+        <h3 className="welcomeSubText">to NC Games</h3>
+      </section>
+    </>
+  );
+}
+
+export default Home;
