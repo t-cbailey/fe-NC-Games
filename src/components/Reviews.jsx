a8aeb7544ecc1fbd94c71587a19bdd36fffa85d7
t-cbailey <t.cbaileymusic@gmail.com>
2023-05-30 14:48:17 +0100

    added loading screen and mobile formatting

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,21 +1,28 @@
 import { fetchReviews } from "../../Utils/fetchUtils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReviewCard from "./ReviewCard";
 
 function Reviews({ SetAllReviews, allReviews }) {
   useEffect(() => {
     fetchReviews().then(({ reviews }) => {
       SetAllReviews(reviews);
+      setIsLoading(false);
     });
   }, []);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <>
       <h2>Reviews</h2>
       <ul className="reviewList">
-        {allReviews.map((review) => {
-          return <ReviewCard key={review.review_id} review={review} />;
-        })}
+        {isLoading ? (
+          <p className="loading">loading...</p>
+        ) : (
+          allReviews.map((review) => {
+            return <ReviewCard key={review.review_id} review={review} />;
+          })
+        )}
       </ul>
     </>
   );
