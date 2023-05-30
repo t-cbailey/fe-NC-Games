9366b46d4b0c963ccce067a874f407606b06caf1
t-cbailey <t.cbaileymusic@gmail.com>
2023-05-30 17:28:52 +0100

    added ability to click review tiles

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 function ReviewCard({ review }) {
+  const navigate = useNavigate();
+
+  function handleClick(event) {
+    event.preventDefault();
+    navigate(`/reviews/${event.target.name}`);
+  }
+
   return (
     <>
-      <section className="reviewCard">
+      <button onClick={handleClick} type="button" className="reviewCard">
         <img
           src={review.review_img_url}
           alt={review.name}
           className="reviewListImage"
+          name={review.review_id}
         />
         <li>{review.title}</li>
         <li>Votes: {review.votes}</li>
         <li>Comments:{review.comment_count}</li>
-      </section>
+      </button>
     </>
   );
 }
