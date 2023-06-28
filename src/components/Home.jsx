abe7e006cc847688cf5a10aa31eeb8d2b6265638
t-cbailey <t.cbaileymusic@gmail.com>
2023-06-28 13:00:51 +0100

    made welcome splash clickable

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import '../../Styling/home.css'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/reviews')
+  }
+
   return (
     <>
-      <section className="welcomeSplash">
+      <section className="welcomeSplash" onClick={handleClick}>
         <h2 className="welcomeText">Welcome</h2>
         <h3 className="welcomeSubText">to NC Games</h3>
+
       </section>
     </>
   );
